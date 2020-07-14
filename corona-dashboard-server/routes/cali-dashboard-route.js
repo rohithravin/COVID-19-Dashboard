e55d24befca5d758f7a573f5789b0c13e33f8be5
Rohith Ravindranath <rohithravin@gmail.com>
2020-07-13 17:32:46 -0700

    update node scripts

--- corona-dashboard-server/routes/cali-dashboard-route.js
+++ corona-dashboard-server/routes/cali-dashboard-route.js
@@ -56,4 +56,22 @@ router.post('/getPlotTotalKind', (req,res) => {
     });
 })
 
+router.post('/getMixedTotalKind', (req,res) => {
+    const data = JSON.parse(req.body['data'])
+    console.log(`cali-dashboard.py --updateMixedPlots [INFO]: Start.`);
+    const subprocess =  spawn('python', ["-u", './scripts/cali-dashboard.py','--updateMixedPlots' , data['plotKindId'], data['plotTraceId'], data['timelineId'] ]);
+    // print output of script
+    subprocess.stdout.on('data', (data) => {
+        console.log(`cali-dashboard.py --updateMixedPlots [INFO]:${data}`);
+        res.send({sucess:100, msg: 'Success.', data: JSON.parse(data.toString().trim())});
+    });
+    subprocess.stderr.on('data', (data) => {
+        console.log(`cali-dashboard.py --updateMixedPlots [ERROR]:${data}`);
+        res.send({sucess:501, msg:data.toString().trim()});
+    });
+    subprocess.on('close', () => {
+        console.log("cali-dashboard.py --updateMixedPlots [INFO]: Closed.");
+    });
+})
+
 module.exports = router;
