494b76ab53de323c4acb0f79f6531965e263a9b6
Rohith Ravindranath <rohithravin@gmail.com>
2020-07-22 19:50:52 -0700

    update ui

--- corona-dashboard/src/app/forecast-dashboard/forecast-dashboard.component.ts
+++ corona-dashboard/src/app/forecast-dashboard/forecast-dashboard.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForecastDashboardComponent implements OnInit {
 
-  constructor() { }
+  showCaliTab:boolean;
+  showUSTab:boolean;
+  showWorldTab:boolean;
+
+  constructor() {
+    this.showCaliTab = true;
+    this.showUSTab = false;
+    this.showWorldTab = false;
+   }
 
   ngOnInit(): void {
   }
 
+  changeForcastTab(tab:string){
+    if (tab == 'california'){
+      this.showCaliTab = true;
+      this.showUSTab = false;
+      this.showWorldTab = false;
+
+    } else if (tab == 'world') {
+      this.showCaliTab = false;
+      this.showUSTab = false;
+      this.showWorldTab = true;
+
+    } else if (tab == 'us'){
+      this.showCaliTab = false;
+      this.showUSTab = true;
+      this.showWorldTab = false;
+    }
+    console.log(tab)
+  }
 }
